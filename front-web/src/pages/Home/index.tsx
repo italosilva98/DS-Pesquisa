c53a9986ac1062b155cf9edc95acd46a57f6c6a9
italosilva98 <italosilvestre3@icloud.com>
2020-09-18 18:26:33 -0300

    Criando navegação com rotas

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
 import { ReactComponent as GammerImage } from '../../assets/gamer.svg';
+import { Link } from 'react-router-dom';
 import './styles.css'
 
 
 const Home = () => (
-    <div className="home-container">
-        <div className="home-text">
-            <h1 className="home-text-title">
-                Quais jogos a galera gosta mais?
+  <div className="home-container">
+    <div className="home-text">
+      <h1 className="home-text-title">
+        Quais jogos a galera gosta mais?
             </h1>
-            <h3 className="home-text-subtitle">
-                Clique no botão abaixo e saibba quais são os jogos que os gamers estão escolhendo!
+      <h3 className="home-text-subtitle">
+        Clique no botão abaixo e saibba quais são os jogos que os gamers estão escolhendo!
             </h3>
 
-            <div className="home-actions">
-                <button className="home-btn">QUERO SABER QUAIS SÃO</button>
-                <div className="home-btn-icon">
-                    <ArrowIcon/>
-                </div>
-            </div>
+      <Link to="/records">
+        <div className="home-actions">
+          <button className="home-btn">QUERO SABER QUAIS SÃO</button>
+          <div className="home-btn-icon">
+            <ArrowIcon />
+          </div>
         </div>
-        <GammerImage className="home-image"/>
+      </Link>
+
     </div>
+    <GammerImage className="home-image" />
+  </div>
 
 );
 
