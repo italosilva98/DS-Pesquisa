5ecbbb7190624bdf9dffa86fee157c0de9146881
italosilva98 <italosilvestre3@icloud.com>
2020-09-19 23:45:58 -0300

    Ajuste no intex.tsx do CreateRecord

--- front-mobile/src/Pages/CreateRecord/index.tsx
+++ front-mobile/src/Pages/CreateRecord/index.tsx
@@ -13,7 +13,8 @@ const placeholder = {
   value: null
 }
 
-const BASE_URL = 'http://192.168.0.12:8080';
+const BASE_URL = 'https://sds1-italo.herokuapp.com';
+
 const mapSelectValue = (games: Game[]) => {
   return games.map(game => ({
     ...game,
